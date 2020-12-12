afa25716f2520789508b5e84efc5c472c89837c7
KaiserPerwez <kaiserperwez@gmail.com>
2020-12-12 13:22:28 +0530

    initial commit

---
+++ app/src/main/java/com/kaiser/gitpublicrepodemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kaiser.gitpublicrepodemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
